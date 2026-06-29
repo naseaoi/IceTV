b8f8d4a2ab5ed9a494a7f2443249f9f7afb342c0
naseaoi <bf5566@qq.com>
2026-06-29 10:42:59 +0800

    fix: use static home revalidate config

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,8 @@
 import HomeClient from '@/features/home/components/HomeClient';
 import { getContinueWatchingSkeletonCount } from '@/lib/continue-watching.server';
-import { HOME_RECOMMENDATION_REVALIDATE_SECONDS } from '@/lib/home-cache';
 import { getHomeInitialData } from '@/lib/home.server';
 
-export const revalidate = HOME_RECOMMENDATION_REVALIDATE_SECONDS;
+export const revalidate = 43200;
 
 export default async function Home() {
   const [initialData, continueWatchingSkeletonCount] = await Promise.all([
